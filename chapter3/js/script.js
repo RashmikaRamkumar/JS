bc1d152f6f6f54405e28306e108c3087809f1470
Rashmika K R <krrashmika2004@gmail.com>
2025-01-07 20:51:47 +0530

    js chapter3

---
+++ chapter3/js/script.js
@@ -0,0 +1,18 @@
+let s="Rashmika";
+console.log(s.length);
+console.log(s.charAt(0));
+console.log(s.charAt(s.length-1));
+console.log(s.indexOf("a")); //first occurance
+console.log(s.lastIndexOf("a")); //last occurance
+console.log(s.slice(4,7)); //start from 4th index and end at 7th index (display 4th,5th,6th index)
+console.log(s.slice(4)); //start from 4th index
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.includes("sh")); //true
+console.log(s.split("a")); //split the string by "a" and display as array
+console.log("Rashmika Ramkumar".split(""));
+console.log("Rashmika Ramkumar".split(" "));
+
+
+//let -- initialise
+//const --constant
