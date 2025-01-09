af823c604b1ca121b451d12cc8c71b222913dca6
Rashmika K R <krrashmika2004@gmail.com>
2025-01-09 19:14:00 +0530

    chapter5 JS

---
+++ chapter5/js/script.js
@@ -0,0 +1,39 @@
+//Math functions
+
+console.log(Math.PI);
+
+console.log(Math.trunc(Math.PI));
+
+console.log(Math.round(4.7));
+console.log(Math.round(4.4));
+
+console.log(Math.ceil(4.4));
+console.log(Math.ceil(4.7));
+
+console.log(Math.floor(4.4));
+console.log(Math.floor(4.7));
+
+console.log(Math.min(4,5,6,7,8,9));
+
+console.log(Math.max(4,5,6,7,8,9));
+
+console.log(Math.random()); //0-1
+console.log(Math.random()*10); //1-10
+console.log(Math.floor(Math.random()*10)); //0-9
+console.log(Math.floor(Math.random()*10)+1); //1-10
+
+console.log(Math.pow(3,4));
+
+//ceil not preferrable here in the random number generation
+console.log(Math.sqrt(16));
+
+console.log(Math.abs(-4.5));
+
+console.log(Math.sin(90));
+
+console.log(Math.cos(90));
+
+console.log(Math.tan(90));
+
+
+
