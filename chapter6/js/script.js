a28b4a2322a2e579af00981ec9a9125005a3bf32
Rashmika K R <krrashmika2004@gmail.com>
2025-01-10 10:40:36 +0530

    chapter6 JS

---
+++ chapter6/js/script.js
@@ -0,0 +1,13 @@
+//take a string generate a letter from it at random
+
+console.log("Rashmika".charAt(Math.floor(Math.random()*8+1)));
+
+console.log("Rashmika".charAt(Math.floor(Math.random()*8+1)));//1-8
+
+console.log("Rashmika".charAt(Math.floor(Math.random()*8)));//0-7
+
+const name="Rashmika";
+
+console.log(name.charAt(Math.floor(Math.random()*name.length)));//one of the best sol
+
+
